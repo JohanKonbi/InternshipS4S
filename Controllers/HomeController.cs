86ee658eecd21df8bfc5c3e929c3956e7d2aa734
JohanKonbi <johantitulaer@gmail.com>
2020-07-21 15:21:19 +0200

    Created Clock, changed Layout and Homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace InternShipPipeline.Controllers
         {
             return View();
         }
+        public IActionResult Clock()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
