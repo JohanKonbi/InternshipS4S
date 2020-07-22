3c1adbf52e33f5d94e79953a2a1f15ac3381df73
Amy Mastwijk <h.m.mastwijk@students.uu.nl>
2020-07-22 10:47:28 +0200

    Add Inlog page (checks username password from .txt file)

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternShipPipeline.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Check()
+        {
+            string line;
+            var username = Request.Form["username"];
+            var password = Request.Form["password"];
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@".\Views\Login\LoginData.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == username + " " + password)
+                {
+                    return RedirectToAction("Clock", "Home");
+                }
+            }
+            return RedirectToAction("Privacy", "Home"); 
+        }
+
+        /*public class User
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CheckLogin
+        {
+            public void ToFile()
+            {
+                var userlist = new List<User>();
+                userlist.Add(new User { Username = "amy", Password = "1234" });
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@"..\LoginData.tx");
+
+                foreach (User s in userlist)
+                {
+                    file.WriteLine(s.Username + " " + s.Password);
+                }
+            }
+        }*/
+    }
+}
