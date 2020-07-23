ff23254b6f6d219c33fc736b51ac25523cc31772
Amy Mastwijk <h.m.mastwijk@students.uu.nl>
2020-07-23 16:15:19 +0200

    One button visible at the time

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,20 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-// Get the container element
-var btnContainer = document.getElementById("myDIV");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        if (current != null) {
-            current[0].className = current[0].className.replace(" active", "");
-            this.className += " active";
-        }
-    });
-}
