b5453d1253bd0863f709f453d0a493b38160d2d3
JohanKonbi <johantitulaer@gmail.com>
2020-07-23 10:25:19 +0200

    Removed Database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,17 +2,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Data.SqlClient;
+using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace InternShipPipeline.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
         public static string username;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+
 
         public IActionResult Index()
         {
