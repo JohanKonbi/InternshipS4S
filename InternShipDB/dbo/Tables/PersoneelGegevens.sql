0e1a448c0e6f4782bce45b5f53faa48f520f7bc3
JohanKonbi <johantitulaer@gmail.com>
2020-07-23 13:12:03 +0200

    Connectie met database gemaakt

---
+++ InternShipDB/dbo/Tables/PersoneelGegevens.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[PersoneelGegevens]
+(
+	[PersoneelID] INT NOT NULL PRIMARY KEY, 
+    [voornaam] NCHAR(10) NULL, 
+    [achternaam] NCHAR(10) NULL, 
+    [wachtwoord] VARCHAR(50) NULL
+)
