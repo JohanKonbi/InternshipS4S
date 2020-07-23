ac057712f2304eb80c31cae27cfebbefd97b0dcb
Amy Mastwijk <h.m.mastwijk@students.uu.nl>
2020-07-23 10:22:17 +0200

    Clock page

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -33,27 +33,27 @@ namespace InternShipPipeline.Controllers
             }
             return Content("Username and/or password are incorrect"); //RedirectToAction("Privacy", "Home"); 
         }
-
-        public class User
-        {
-            public string Username { get; set; }
-            public string Password { get; set; }
-        }
-        /*
-        public class CheckLogin
-        {
-            public void ToFile()
-            {
-                var userlist = new List<User>();
-                userlist.Add(new User { Username = "amy", Password = "1234" });
-
-                System.IO.StreamWriter file = new System.IO.StreamWriter(@"..\LoginData.tx");
-
-                foreach (User s in userlist)
-                {
-                    file.WriteLine(s.Username + " " + s.Password);
-                }
-            }
-        }*/
     }
+
+    /*public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class CheckLogin
+    {
+        public void ToFile()
+        {
+            var userlist = new List<User>();
+            userlist.Add(new User { Username = "amy", Password = "1234" });
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"..\LoginData.tx");
+
+            foreach (User s in userlist)
+            {
+                file.WriteLine(s.Username + " " + s.Password);
+            }
+        }
+    }*/
 }
