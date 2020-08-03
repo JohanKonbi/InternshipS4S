84e85bdbef7df674dde1656fc30ed9092785e23b
JohanKonbi <johantitulaer@gmail.com>
2020-08-03 13:51:24 +0200

    Weg met oude Function

--- Functions/Function1.cs
+++ Functions/Function1.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
